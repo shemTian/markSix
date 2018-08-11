e4d30cff9c2577d2f6973d05c8567c0090080323
shem <shemtian@gmail.com>
2018-08-12 02:44:22 +0800

    modify server port

--- src/main/java/com/tsq/read/controller/Index.java
+++ src/main/java/com/tsq/read/controller/Index.java
@@ -1,8 +1,16 @@
 package com.tsq.read.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by tsq on 2:15.
  * VERSION 1.0.0
  */
+@Controller
 public class Index {
+    @GetMapping({"/","index","index.html","index.htm"})
+    public String index() {
+        return "index";
+    }
 }

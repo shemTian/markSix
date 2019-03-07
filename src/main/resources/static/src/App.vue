524e59ba5eccaa1edf92455d24d6d9f1aa059cad
shemtian@gmail.com <shemtian@gmail.com>
2019-03-07 11:17:59 +0800

    init vue+element-ui

--- src/main/resources/static/src/App.vue
+++ src/main/resources/static/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <Header></Header>
+    <div id="app">
+      <img src="./assets/logo.png">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
-  name: 'App'
-}
+  components: { Header },
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/main/resources/static/src/components/Header.vue
@@ -0,0 +1,9 @@
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+<!-- <script src="./header.js"></script> -->
+<template src="./header.html"></template>
+<style lang="less" src="./header.less" scoped></style>

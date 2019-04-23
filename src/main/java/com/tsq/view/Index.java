2ae480863591a1a12a4f7aff93e4a6594bb072f0
shemtian <shemtian@gmail.com>
2019-04-23 18:24:13 +0800

    for ng load-balance test

--- src/main/java/com/tsq/view/Index.java
+++ src/main/java/com/tsq/view/Index.java
@@ -1,7 +1,10 @@
 package com.tsq.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by tsq on 2:15.
@@ -9,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class Index {
+
+    @Autowired
+    Environment environment;
     @GetMapping({"/","index","index.ftl","index.htm"})
+    @ResponseBody
     public String index() {
-        return "index";
+        String property = environment.getProperty("server.port");
+        return property;
     }
 }
